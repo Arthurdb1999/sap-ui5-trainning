8febceb2e590a3c07e49d52647855d118b70aabf
arthurdb1999 <arthurdb1999@gmail.com>
2022-03-01 20:35:30 -0300

    Step 9: Component Configuration - Get started, Walkthrough SAPUI5

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,23 @@
+sap.ui.define([
+    // Bibliotecas utilizadas
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+], (Controller, MessageToast) => {
+    "use strict"
+
+    // Definição do controller
+    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+
+        onShowHello: function () {
+            // ler uma mensagem do model i18n
+            // o arquivo i18n me parece com o arquivo strings.xml que existe na criação de projetos Android Kotlin
+            let oBundle = this.getView().getModel("i18n").getResourceBundle()
+            let sRecipient = this.getView().getModel().getProperty("/recipient/name")
+            let sMsg = oBundle.getText("helloMsg", [sRecipient])
+
+            // mostrar a mensagem
+            MessageToast.show(sMsg)
+        },
+        
+    })
+})
